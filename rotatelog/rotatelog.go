8d9f6f3715f4299bdaaea7c2793affd0f40e87b1
董家宏 <dongjiahong@hotmail.com>
2017-01-01 13:04:33 +0800

    add rotate

--- rotatelog/rotatelog.go
+++ rotatelog/rotatelog.go
@@ -6,21 +6,27 @@ import (
 	"os"
 	"path/filepath"
 	"time"
+	"fmt"
+	"sync"
 )
 
 type RotateLog struct {
-	logPath    string
+	logAbsPath     string
 	logPreffix string
 	flag       int
 	fp         *os.File
 	logger     *log.Logger
 	rorateTime time.Duration
+	rotateMutex sync.RWMutex
 }
 
 // NewRotateLog creat a object of RotateLog
 func NewRotateLog(path string, prefix string, flag int) (*RotateLog, error) {
 	if len(path) == 0 {
 		return nil, errors.New("NewRotateLog path is nil")
+	}
+	if len(prefix) != nil {
+		prefix += " "
 	}
 	absPath, err := filepath.Abs(path)
 	var fp *os.File
@@ -29,7 +35,7 @@ func NewRotateLog(path string, prefix string, flag int) (*RotateLog, error) {
 		log.Fatalln("NewRotateLog open abs path err: ", err.Error())
 	}
 	return &RotateLog{
-		logPath:    path,
+		logAbsPath:    absPath,
 		logPreffix: prefix,
 		fp:         fp,
 		logger:     log.New(fp, prefix, flag),
@@ -42,6 +48,25 @@ func (r *RotateLog) Println(args ...interface{}) {
 	r.logger.Println(args)
 }
 
+func (r *RotateLog) SetRotateTime(t time.Duration) {
+	r.rorateTime = t
+}
+
+func (r *RotateLog) rotateWithLock() {
+	r.rotateMutex.Lock()
+	defer r.rotateMutex.Unlock()
+
+	if stat. err := os.Stat(r.logAbsPath)
+	// empty file don't rotate
+	if stat.Size() == 0 {
+		return
+	}
+}
+
 func main() {
-	log.Println("aabbcc")
+	rl, err := NewRotateLog("logs/rotatelog", "[NewRotate]", log.LUTC|log.LstdFlags)
+	if err != nil {
+		fmt.Println("get new rotate log err: ", err)
+	}
+	rl.logger.Println("12345")
 }
